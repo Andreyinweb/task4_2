364666bd739804183072e77651ecd48e1134fb67
AndreyInWeb <andreyinweb@gmail.com>
2018-04-01 01:43:16 +0300

    Development1

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -18,19 +18,7 @@ fi
 
 if [ "$( diff -q /etc/ntp.conf.bak /etc/ntp.conf )" ] ; then
     echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
-
     echo "$( diff -u -B /etc/ntp.conf.bak /etc/ntp.conf)"
-
-    # echo "--- /etc/ntp.conf.bak $(stat -c%z /etc/ntp.conf.bak)"
-    # echo "+++ /etc/ntp.conf     $(stat -c%z /etc/ntp.conf)"
-
-    # msstr=$( diff -B /etc/ntp.conf.bak /etc/ntp.conf | sed 's/</-/' | sed 's/>/+/' | grep '\- '| sed 's/- //' | wc -m )
-    # plusstr=$( diff -B /etc/ntp.conf.bak /etc/ntp.conf | sed 's/</-/' | sed 's/>/+/' | grep '+ '| sed 's/+ //' | wc -m )
-    # echo "@ -$(expr $msstr - 1) +$(expr $plusstr - 1) @" 
-    
-    # diff -B /etc/ntp.conf.bak /etc/ntp.conf | sed 's/</-/' | sed 's/>/+/' | grep '\- '
-    # diff -B /etc/ntp.conf.bak /etc/ntp.conf | sed 's/</-/' | sed 's/>/+/' | grep '+ ' 
     cat /etc/ntp.conf.bak > /etc/ntp.conf
-
     sudo service ntp restart
 fi
