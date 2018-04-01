5901f4a8a23f6e9803e9dfd96fa6c67888ac95d5
AndreyInWeb <andreyinweb@gmail.com>
2018-04-01 11:47:50 +0300

    Added_two_checks

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+# Permission to run executable code. Разрешение на запуск
 chmod +x ntp_deploy.sh
 chmod +x ntp_verify.sh
-
+# Specifies the current folder. Определяет текущую папку
 wayprog=$(pwd)
-
+# Checks ntp package. Проверяет наличее пакета ntp
 indpkg=$( dpkg --get-selections | grep -m 1 'ntp' | awk '{print $2}' )
 if [ -n "$indpkg" ] ; then
+    # Checks if the package is installed. Проверяет установлен ли пакет
     if [[ "$indpkg" != "install" ]] ; then
         sudo apt-get -y install ntp
     fi
 else
+    # Installs the package. Устанавливает пакет
     sudo apt-get -y install ntp
 fi
-
+# Checks ntp.conf. Проверяет наличее ntp.conf
 if ! [ -f /etc/ntp.conf ] ;then 
     echo "Error: ntp_deploy: There is no /etc/ntp.conf file" >&2 
     exit 1
 fi
-
+# Modifies ntp.conf. Изменяет ntp.conf
 ( sed '/pool 0/ c\pool 0.ua.pool.ntp.org' /etc/ntp.conf ) > temp.txt
 ( sed '/pool 1/ c\pool 1.ua.pool.ntp.org' temp.txt ) > /etc/ntp.conf.bak
 ( sed '/pool 2/ c\pool 2.ua.pool.ntp.org' /etc/ntp.conf.bak ) > temp.txt
@@ -28,19 +31,19 @@ cat temp.txt > /etc/ntp.conf.bak
 rm -f temp.txt
 
 cat /etc/ntp.conf.bak > /etc/ntp.conf
-
+# Restarts ntp. Перезапускает ntp
 sudo service ntp restart
 
 # ntp_verify.sh
-
+# Adds a task to the scheduler. Дописывает задание в планировщик
 sudo echo > /var/spool/cron/root
 echo "*/1 * * * * $wayprog/ntp_verify.sh" >> /var/spool/cron/root
 echo >> /var/spool/cron/root
 
 crontab -u root /var/spool/cron/root
-
+# Restarts the scheduler. Перезапускает планировщик
 service cron reload
-
+# Checks for the presence of ntp_verify.sh  Проверяет наличие ntp_verify.sh
 if [ -f ntp_verify.sh ] ; then
     exit 0
 else
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-############################# del 
-exec 4<&1
-exec 1>/home/stariy/andrey/DevOps/task4_2/task4_2.out
-echo "$(date)" >> /home/stariy/andrey/DevOps/task4_2/task4_2.out
-exec 5<&2
-exec 2>/home/stariy/andrey/DevOps/task4_2/errors4_2.log 
-############################# del
 
+
+# Checking the operation of ntp. Проверка работы ntp 
 if ! [ "$( ntpq -p )" ] ; then
     echo "NOTICE: ntp is not running" 
     sudo service ntp start
 fi
- 
+# Check for ntp.conf.bak Error, exit. Проверка наличия ntp.conf.bak Ошибка, выход.
 if ! [ -f /etc/ntp.conf.bak ] ; then 
     echo "Error: ntp_verify: no file /etc/ntp.conf.bak" >&2
     exit 1
 fi
-
+# Check for ntp.conf. Overwriting. Проверка наличия ntp.conf. Перезапись
 if ! [ -f /etc/ntp.conf ] ; then 
     echo "NOTICE: No file '/etc/ntp.conf' " 
     cat /etc/ntp.conf.bak > /etc/ntp.conf
 fi
-
+# Verifying the Conformity of Configuration Files. Проверка соответствий файлов конфигурации
 if [ "$( diff -q /etc/ntp.conf.bak /etc/ntp.conf )" ] ; then
     echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
     echo "$( diff -u -B /etc/ntp.conf.bak /etc/ntp.conf)"
     cat /etc/ntp.conf.bak > /etc/ntp.conf
     sudo service ntp restart
+fi
+# Second check (after overwriting) of configuration file matches.
+# Вторая проверка (после перезаписи) соответствий файлов конфигурации
+if [ "$( diff -q /etc/ntp.conf.bak /etc/ntp.conf )" ] ; then
+    echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
+    echo "$( diff -u -B /etc/ntp.conf.bak /etc/ntp.conf)"
+    cat /etc/ntp.conf.bak > /etc/ntp.conf
+    sudo service ntp restart
+fi
+# Error . Не удалось изменить файл ntp.conf
+if [ "$( diff -q /etc/ntp.conf.bak /etc/ntp.conf )" ] ; then
+    echo "Error: ntp_verify: Can not modify ntp.conf file" >&2
+    exit 1
 fi
